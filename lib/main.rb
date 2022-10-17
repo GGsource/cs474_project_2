c6d8611d91a15b5a7e711be9e181b8b0c1332c71
ggonza55 <ggonza55@uic.edu>
2022-10-17 11:20:21 -0500

    Initial Upload

---
+++ lib/main.rb
@@ -0,0 +1,6 @@
+#############################################################################################
+# Author: Geo Gonzalez
+# Name: main.rb
+# Date: October 17th 2022
+# Description: This is the main file for Project 2: Building an Assembly Language Interpreter
+#############################################################################################
